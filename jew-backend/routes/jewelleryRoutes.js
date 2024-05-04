32147c84b290f488ba58a176a9da43569ce86677
Nishant <nishantdewangan2002@gmail.com>
2024-05-04 18:32:47 +0530

    final 2.3

--- jew-backend/routes/jewelleryRoutes.js
+++ jew-backend/routes/jewelleryRoutes.js
@@ -1,11 +1,9 @@
 
 const express = require('express');
-const cors= require('cors');
 const router = express.Router();
 router.use(express.json())
 const jewelleryController = require('../controllers/jewelleryController');
-const app = express();
-app.use(cors());
+
 // Define user routes
 router.post('/add',jewelleryController.insertJewellry);
 router.post('/bymerchant',jewelleryController.getJewelryByMerchant);
